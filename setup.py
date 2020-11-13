9e511237ae6d6d2550b2041b4fb5760b8fd1e11e
GouMinghao <gmh2015@163.com>
2020-11-13 13:16:08 +0800

    update

---
+++ setup.py
@@ -0,0 +1,18 @@
+# // Code Written by Minghao Gou
+
+from distutils.core import setup
+
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    version='1.0.0',
+    description='Grasp NMS',
+    author='Minghao Gou',
+    author_email='gouminghao@gmail.com',
+    url='https://github.com/GouMinghao/graspnms',
+    name = 'grasp_nms',
+    ext_modules=cythonize("grasp_nms.pyx"),
+    include_dirs=[numpy.get_include()],
+    install_requires=['cython', 'numpy']
+)
